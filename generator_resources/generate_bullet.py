93101ac587a1fa720222b4680d10d69fab596b37
root <root@DESKTOP-2VPR9IB.>
2025-03-21 17:19:15 +0100

    sherlock

--- generator_resources/generate_bullet.py
+++ generator_resources/generate_bullet.py
@@ -1,6 +1,7 @@
-movies=["Rise of the Planet of the Apes", "Dawn of the Planet of the Apes", "Death on the Nile", "La sociedad de la Nieve", "War for the Planet of the Apes", "All quiet on the Western Front", 
+movies_old=["Rise of the Planet of the Apes", "Dawn of the Planet of the Apes", "Death on the Nile", "La sociedad de la Nieve", "War for the Planet of the Apes", "All quiet on the Western Front", 
         "Poor Things", "Dune: Part Two", "Marriage Story", "Into the Wild", "As Bestas", "Everything Everywhere All at Once", "Smoking Causes Coughing", "The Dead Don't Die", "Hannibal", "Anatomy of a Fall", "A Haunting in Venice",
           "The Gentlemen", "The Man from U.N.C.L.E.", "Que Dios nos Perdone", "Wrath of Man", "Spaceman"]
+movies=["Sherlock Holmes"]
 
 for movie in movies:
     resource = movie.replace(" ", "-")
